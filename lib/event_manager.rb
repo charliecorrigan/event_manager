ebc2fc04fb5e02d983693616ac20800a5d26f9f8
ccorrigan <barnumswinter@gmail.com>
2017-04-08 09:17:59 -0600

    Iteration 3

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,18 @@
 require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  legislator_names = legislators.map do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+  legislator_names.join(", ")
 end
 
 puts "Event Manager Initialized! Let's Go!"
@@ -10,6 +21,8 @@ contents = CSV.open('event_attendees.csv', headers: true, header_converters: :sy
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
 
