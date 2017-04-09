481f72dd43d47891c312c41e20015ae0cef38192
ccorrigan <barnumswinter@gmail.com>
2017-04-09 08:57:44 -0600

    Clean phone numbers Iteration

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -20,6 +20,38 @@ def save_thank_you_letters(id, form_letter)
   end
 end
 
+def is_digit?(character)
+  character =~ /[[:digit:]]/
+end
+
+def remove_non_digit_characters(phone_number)
+  clean_number = phone_number.split("").map do |character|
+    if is_digit?(character)
+      character
+    else
+      next
+    end
+  end
+  clean_number.join
+end
+
+def phone_number_is_valid?(phone_number)
+  return true if phone_number.length == 10
+  return false if phone_number.length < 10 || phone_number.length > 11
+  return true if phone_number[0] == "1"
+  return false if phone_number[0] != "1"
+end
+
+def clean_phone_number(phone_number)
+  phone_number = remove_non_digit_characters(phone_number)
+  if phone_number_is_valid?(phone_number)
+    return phone_number[1..10] if phone_number.length == 11
+    return phone_number
+  else
+    return nil
+  end
+end
+
 puts "Event Manager Initialized! Let's Go!"
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
@@ -31,7 +63,9 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  phone_number = clean_phone_number(row[:homephone])
 
+  puts phone_number
   form_letter = erb_template.result(binding)
   save_thank_you_letters(id, form_letter)
   
