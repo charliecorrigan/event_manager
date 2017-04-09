94c661df798afac5fd50a987096d01af7f217251
ccorrigan <barnumswinter@gmail.com>
2017-04-09 08:10:03 -0600

    Iteration 4

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -9,20 +10,32 @@ end
 
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legislator_names = legislators.map do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir('output') unless Dir.exists?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
   end
-  legislator_names.join(", ")
 end
 
 puts "Event Manager Initialized! Let's Go!"
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new(template_letter)
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id, form_letter)
+  
 end
+
+
 
