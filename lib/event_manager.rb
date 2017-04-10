e8f55a7d7a99264b9257a344f0289391615c3077
ccorrigan <barnumswinter@gmail.com>
2017-04-10 07:10:38 -0600

    Add final iteration

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -58,9 +58,21 @@ def isolate_registration_hour(registration_time)
   time.strftime('%H')
 end
 
-def find_most_popular(registration_hours)
-  registration_hours.max_by do |hour|
-    registration_hours.count(hour)
+def isolate_registration_day(registration_day)
+  day = DateTime.strptime(registration_day, '%m/%d/%y %k:%M')
+  day_of_the_week = day.wday
+  return "Sunday" if day_of_the_week == 0
+  return "Monday" if day_of_the_week == 1
+  return "Tuesday" if day_of_the_week == 2
+  return "Wednesday" if day_of_the_week == 3
+  return "Thursday" if day_of_the_week == 4
+  return "Friday" if day_of_the_week == 5
+  return "Saturday" if day_of_the_week == 6
+end
+
+def find_most_popular(registration_info)
+  registration_info.max_by do |item|
+    registration_info.count(item)
   end
 end
 
@@ -74,6 +86,16 @@ def find_most_popular_registration_hour(contents)
   most_popular_hour.strftime('%l:%M %p')
 end
 
+def find_most_popular_registration_day(contents)
+  
+  registration_days = contents.map do |row|
+    registration_day = row[:regdate]
+    #binding.pry
+    isolate_registration_day(registration_day)
+  end
+  find_most_popular(registration_days)
+end
+
 puts "Event Manager Initialized! Let's Go!"
 
 contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
@@ -81,21 +103,24 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new(template_letter)
 
 most_popular_hour = find_most_popular_registration_hour(contents)
-
 puts "The most popular registration hour is #{most_popular_hour}"
+
+contents.rewind
+
+most_popular_day = find_most_popular_registration_day(contents)
+puts "The most popular registration day is #{most_popular_day}"
+
+contents.rewind
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   phone_number = clean_phone_number(row[:homephone])
-  registration_time = row[:regdate]
-  registration_hour = isolate_registration_hour(registration_time)
-  puts registration_hour
 
   form_letter = erb_template.result(binding)
   save_thank_you_letters(id, form_letter)
-  
 end
 
 
